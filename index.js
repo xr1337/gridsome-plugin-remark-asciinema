1d26a78e2965911f8585fc0bf8df732814fe9dd7
sufiyan <xr1337@gmail.com>
2020-06-16 09:25:10 +0800

    First commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const visit = require("unist-util-visit");
+
+const urlRegexp = /https:\/\/asciinema\.org\/a\/([A-Za-z0-9-_]*)\.js/i;
+
+const isAsciinemaLink = node => {
+  return (
+    node.children.length === 1 &&
+    node.children[0].type === "link" &&
+    (urlRegexp.exec(node.children[0].url))
+  );
+};
+
+module.exports = options => {
+  return async tree => {
+    const nodes = [];
+    visit(tree, "paragraph", node => {
+      const tagId = isAsciinemaLink(node)
+      if (tagId) {
+        nodes.push([node, tagId[1]]);
+      }
+    });
+
+    for (let i = 0; i < nodes.length; i++) {
+      const nt = nodes[i];
+      const node = nt[0];
+      const asciinemaLink = nt[1];
+      try {
+        node.type = "html";
+        node.value = `<script id="asciicast-${asciinemaLink}" src="https://asciinema.org/a/${asciinemaLink}.js" async></script>`;
+      } catch (err) {
+      }
+    }
+  };
+};
